f4bbc1f4bcd0d23e92f389fe1b9575af2863e573
Bernard Peh <bpeh@sitecritic.net>
2018-08-18 12:01:38 +1000

    add main-v4

---
+++ src/main-v4.js
@@ -0,0 +1,113 @@
+const CryptoJs= require("crypto-js");
+
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
+class Block {
+    constructor(timestamp, transactions, previousHash = '') {
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.transactions = transactions;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return CryptoJs.SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("BLOCK MINED: " + JSON.stringify(this));
+    }
+}
+
+
+class Blockchain{
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 3;
+        this.pendingTransactions = [];
+        this.miningReward = 12.5;
+    }
+
+    createGenesisBlock() {
+        return new Block(Date.now(), [], "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    minePendingTransactions(miningRewardAddress){
+
+        // coinbase transaction
+        this.pendingTransactions.unshift(new Transaction(null, miningRewardAddress, this.miningReward));
+
+        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+        block.mineBlock(this.difficulty);
+
+        console.log('Block successfully mined!');
+        this.chain.push(block);
+
+    }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+let savjeeCoin = new Blockchain();
+savjeeCoin.createTransaction(new Transaction('address1', 'address2', 40));
+savjeeCoin.createTransaction(new Transaction('address2', 'address1', 10));
+
+console.log('\n Starting TMA miner...');
+savjeeCoin.minePendingTransactions('tma-miner');
+
+console.log('\nBalance of address1 is', savjeeCoin.getBalanceOfAddress('address1'));
+console.log('\nBalance of address2 is', savjeeCoin.getBalanceOfAddress('address2'));
+console.log('\nBalance of tma-miner is', savjeeCoin.getBalanceOfAddress('tma-miner'));
